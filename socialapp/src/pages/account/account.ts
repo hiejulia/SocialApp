01e142a7001b844a3156db188a01ae677abfe8a4
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-04-11 13:11:51 +0300

    add txt file

--- socialapp/src/pages/account/account.ts
+++ socialapp/src/pages/account/account.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams,ActionSheet } from 'ionic-angular';
+import {FirebaseAuthConfig,FirebaseAuthState} from 'angularfire2';
+import {UtilProvider} from '../../providers/utils';
+import {UserProvider} from '../../providers/user-provider/user-provider';
+import {SocialProvider} from '../../providers/social-provider/social-provider';
 
 /*
   Generated class for the Account page.
@@ -13,10 +17,37 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class AccountPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  user= {};
+  profile:Object = {};
+
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,) {
+
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AccountPage');
   }
+
+
+  //updateProfile
+
+
+  //log out
+  logout(){
+    this.afAuth.logout();
+  }
+
+  //update picture
+  updatePicture(){
+    this.present
+  }
+
+  updateProfile(){
+    let toast = 
+  }
+
+
+
 
 }
